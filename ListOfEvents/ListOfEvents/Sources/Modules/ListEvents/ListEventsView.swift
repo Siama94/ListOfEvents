a598667c2b0d69dec9fe789311f71d34971d0067
Siama94 <sia.m94@ya.ru>
2023-02-07 20:14:09 +0500

    Create files for list of events.

--- ListOfEvents/ListOfEvents/Sources/Modules/ListEvents/ListEventsView.swift
+++ ListOfEvents/ListOfEvents/Sources/Modules/ListEvents/ListEventsView.swift
@@ -5,7 +5,7 @@
 //  Created by Anastasiia iOS on 07.02.2023.
 //
 
+import UIKit
 
 final class PartnersView: RxBaseView {
-
 }
---
+++ ListOfEvents/ListOfEvents/Sources/Modules/ListEvents/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  ListOfEvents
+//
+//  Created by Anastasiia iOS on 07.02.2023.
+//
+
+import Foundation
